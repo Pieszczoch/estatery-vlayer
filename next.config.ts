0dd473fa2eaeba9479e1eea23cbba801714c1eee
sebastian.kopacki <sebastian.kopacki5@gmail.com>
2025-04-20 17:45:41 +0200

    up

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,45 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find(
+      (rule: { test: { test: (arg0: string) => any } }) =>
+        rule.test?.test?.(".svg")
+    );
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              icon: true,
+              expandProps: true,
+              svgProps: {
+                width: "{props.width || 24}",
+                height: "{props.width || 24}",
+              },
+              replaceAttrValues: {
+                "#333": "{props.color || 'currentColor'}",
+              },
+            },
+          },
+        ],
+      }
+    );
+    fileLoaderRule.exclude = /\.svg$/i;
+    return config;
+  },
   /* config options here */
 };
 
