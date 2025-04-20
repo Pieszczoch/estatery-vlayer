108603d6961aeb4d4d2f93ab7b8f0d9a5dac873e
Sebastian Kopacki <sebastian.kopacki5@gmail.com>
2025-04-20 15:51:50 +0000

    Rm trashes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,47 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find(
+      (rule: { test: { test: (arg0: string) => any } }) =>
+        rule.test?.test?.(".svg")
+    );
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              icon: true,
+              expandProps: true,
+              svgProps: {
+                width: "{props.width || 24}",
+                height: "{props.width || 24}",
+              },
+              replaceAttrValues: {
+                "#333": "{props.color || 'currentColor'}",
+              },
+            },
+          },
+        ],
+      }
+    );
+    fileLoaderRule.exclude = /\.svg$/i;
+    return config;
+  },
+
+  images: {
+    dangerouslyAllowSVG: true,
+  },
 };
 
 export default nextConfig;
