0571ee695a02de3fde14d70c2dad4f8f69a15605
sebastian.kopacki <sebastian.kopacki5@gmail.com>
2025-04-22 17:44:06 +0200

    wip

---
+++ src/components/ui/Tabs/index.ts
@@ -0,0 +1,15 @@
+import {
+  Tabs as Wrapper,
+  TabsList as List,
+  TabsTrigger as Trigger,
+  TabsContent as Content,
+} from "./Tabs";
+
+const Tabs = {
+  Wrapper,
+  List,
+  Trigger,
+  Content,
+};
+
+export default Tabs;
